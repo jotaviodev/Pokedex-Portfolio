037fae7157fac8a41dda19f2b2eaa4454d74b3b3
joao.matos1 <joao.matos@cwi.com.br>
2026-05-13 10:36:42 -0300

    sw

--- sw.js
+++ sw.js
@@ -1,11 +1,12 @@
-const CACHE_NAME = 'pokedex-v1';
-
+const CACHE_NAME = 'pokedex-v2';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
   './manifest.json',
+  './icons/icon-192.png',   
+  './icons/icon-512.png',   
 ];
 
 self.addEventListener('install', event => {
